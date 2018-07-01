7cb50a29d90eb7657b46ca0a01d3b5c7309e066d
WEHXZL <fuyuanwyyx@163.com>
2018-07-01 15:13:49 +0800

    提交文件

---
+++ src/main/java/com/bdqn/sbproject/SbprojectApplication.java
@@ -0,0 +1,12 @@
+package com.bdqn.sbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbprojectApplication.class, args);
+	}
+}
