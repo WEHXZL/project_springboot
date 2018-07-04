25d5efd53b3b89f1994f118e1b3202e0e908e6eb
WEHXZL <fuyuanwyyx@163.com>
2018-07-04 17:00:57 +0800

    测试上传

--- src/main/java/com/bdqn/sbproject/util/Page.java
+++ src/main/java/com/bdqn/sbproject/util/Page.java
@@ -5,4 +5,36 @@ public class Page {
   private Integer pageSize;
   private Integer rowCount;
   private Integer fromTo;
+
+  public Integer getPageIndex() {
+    return pageIndex;
+  }
+
+  public void setPageIndex(Integer pageIndex) {
+    this.pageIndex = pageIndex;
+  }
+
+  public Integer getPageSize() {
+    return pageSize;
+  }
+
+  public void setPageSize(Integer pageSize) {
+    this.pageSize = pageSize;
+  }
+
+  public Integer getRowCount() {
+    return rowCount;
+  }
+
+  public void setRowCount(Integer rowCount) {
+    this.rowCount = rowCount;
+  }
+
+  public Integer getFromTo() {
+    return fromTo;
+  }
+
+  public void setFromTo(Integer fromTo) {
+    this.fromTo = fromTo;
+  }
 }
