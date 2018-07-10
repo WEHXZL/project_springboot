51ff7b10c5429ec2281202e87b6542ea3a49300a
WEHXZL <fuyuanwyyx@163.com>
2018-07-10 13:35:03 +0800

    测试上传

--- src/main/java/com/bdqn/sbproject/util/Page.java
+++ src/main/java/com/bdqn/sbproject/util/Page.java
@@ -1,11 +1,5 @@
 package com.bdqn.sbproject.util;
 
 public class Page {
-  private Integer pageIndex;
-  private Integer pageSize;
-  private String studentName;
-  public String studentID;
 
-//dfghjklrtyuiodfghjkl
-  //富源傻逼  傻逼富源
 }
